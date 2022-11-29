29f0ea7cca92da2c857c9cfc393bf9b562082187
Vladislav Ivanov <103201559+DesperatePerson@users.noreply.github.com>
2022-11-29 22:24:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll(".slide");
+
+function slidesPlugin(activeSlide) {
+  slides[activeSlide].classList.add("active");
+
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+
+      slide.classList.add("active");
+    });
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => slide.classList.remove("active"));
+  }
+}
+
+slidesPlugin(2);
